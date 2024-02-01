5b1ce5a0226563a80c7548681701dc4d4aedd119
Mikhail326 <misha.stelmakh1995@gmail.com>
2024-02-01 14:54:57 +0300

    create update delete exercise

--- app/exercise/exercise.controller.js
+++ app/exercise/exercise.controller.js
@@ -22,7 +22,52 @@ export const createNewExercise = asyncHandler(async (req, res) => {
 // @route GET /api/exercises/
 // @access Private
 export const allExercises = asyncHandler(async (req, res) => {
-	const exercises = await prisma.exercise.findMany()
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createAt: 'desc'
+		}
+	})
 
 	res.json(exercises)
+})
+
+// @desc Update exercise
+// @route PUT /api/exercises/:id
+// @access Private
+export const updateExercise = asyncHandler(async (req, res) => {
+	try {
+		const { name, times, pathIcon } = req.body
+
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: Number(req.params.id)
+			},
+			data: {
+				name,
+				times,
+				pathIcon
+			}
+		})
+
+		res.json(exercise)
+	} catch (e) {
+		res.status(404).json({ message: 'Упражнение не найдено!' })
+	}
+})
+
+// @desc Delete exercise
+// @route Delete /api/exercises/:id
+// @access Private
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		await prisma.exercise.delete({
+			where: {
+				id: Number(req.params.id)
+			}
+		})
+
+		res.json({ message: 'Упражнение удалено!' })
+	} catch (e) {
+		res.status(404).json({ message: 'Упражнение не найдено!' })
+	}
 })
--- app/exercise/exercise.routes.js
+++ app/exercise/exercise.routes.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import { checkedAuth } from '../middleware/auth.middleware.js'
-import { createNewExercise, allExercises } from './exercise.controller.js'
+import { createNewExercise, allExercises, updateExercise, deleteExercise } from './exercise.controller.js'
 
 const router = express.Router()
 
 router.route('/').post(checkedAuth, createNewExercise)
 router.route('/').get(checkedAuth, allExercises)
+router.route('/:id').put(checkedAuth, updateExercise)
+router.route('/:id').delete(checkedAuth, deleteExercise)
 
 export default router
